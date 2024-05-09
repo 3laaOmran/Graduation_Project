e2eddfa102343ea0d1545891ff22c028ebc74d67
3laaOmran <115973437+3laaOmran@users.noreply.github.com>
2024-05-09 23:25:06 +0300

    edit about app screen

--- two_way_dael/lib/features/home/ui/Modules/about_app.dart
+++ two_way_dael/lib/features/home/ui/Modules/about_app.dart
@@ -43,101 +43,91 @@ class _AboutAppScreenState extends State<AboutAppScreen> {
               image: AssetImage('assets/images/main_background.png'),
               fit: BoxFit.cover),
         ),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                verticalSpace(70),
-                AnimatedTextKit(
-                  repeatForever: true,
-                  animatedTexts: [
-                    WavyAnimatedText(
-                      'Two Way Deal ',
-                      textStyle: TextStyles.font30blackbold,
-                    ),
-                  ],
-                ),
-                verticalSpace(5),
-                Text(
-                  'Version 0.1',
-                  style: TextStyles.font13GreyBold,
-                ),
-                verticalSpace(30),
-                const Image(
-                  image: AssetImage('assets/images/two_way_deal_icon.png'),
-                ),
-                verticalSpace(30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.copyright,
-                      color: Colors.grey,
-                    ),
-                    horizontalSpace(5),
-                    Text(
-                      '2Way Deal Flutter Team',
-                      style: TextStyles.font13GreyBold,
-                    ),
-                  ],
-                ),
-                verticalSpace(30),
-                AppTextButton(
-                  horizontalPadding: 10,
-                  verticalPadding: 10,
-                  buttonWidth: double.maxFinite,
-                  buttonText: 'About Us',
-                  textStyle: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                  onPressed: () {
-                    setState(() {
-                      aboutUs = !aboutUs;
-                    });
-                  },
-                ),
-                verticalSpace(20),
-                aboutUs
-                    ? Column(
-                        children: [
-                          BuildAnimatedTextItem(
-                            text: '2Way Deal Overview :-',
-                            textStyle: TextStyles.font17BlackBold,
-                          ),
-                          BuildAnimatedTextItem(
-                            text:
-                                'Our application aims to reduce food waste by selling it at lower price than its original cost or donating it. We strive to minimize food loss and promote affordability, benefiting both consumers and the environment.',
-                            textStyle: TextStyles.font15Black45Medium,
-                          ),
-                          verticalSpace(10),
-                          BuildAnimatedTextItem(
-                            text: 'Our Goals :-',
-                            textStyle: TextStyles.font17BlackBold,
-                          ),
-                          BuildAnimatedTextItem(
-                            text:
-                                '1- Reduce food waste by 30% within the first year of implementation, as measured by the total weight of unsold food items.',
-                            textStyle: TextStyles.font15Black45Medium,
-                          ),
-                          verticalSpace(5),
-                          BuildAnimatedTextItem(
-                            text:
-                                '2- Increase the accessibility of affordable food options by offering discounted prices, leading to 20% increase in customer.',
-                            textStyle: TextStyles.font15Black45Medium,
-                          ),
-                          verticalSpace(5),
-                          BuildAnimatedTextItem(
-                            text:
-                                '3- participation within six months. Collaborate with local food banks and charities to donate surplus food, aiming to distribute a minimum of 500 meals per month to those in need.',
-                            textStyle: TextStyles.font15Black45Medium,
-                          ),
-                        ],
-                      )
-                    : Container(),
-              ],
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  verticalSpace(70),
+                  AnimatedTextKit(
+                    repeatForever: true,
+                    animatedTexts: [
+                      WavyAnimatedText(
+                        'Two Way Deal ',
+                        textStyle: TextStyles.font30blackbold,
+                      ),
+                    ],
+                  ),
+                  verticalSpace(5),
+                  Text(
+                    'Version 0.1',
+                    style: TextStyles.font13GreyBold,
+                  ),
+                  verticalSpace(30),
+                  const Image(
+                    image: AssetImage('assets/images/two_way_deal_icon.png'),
+                  ),
+                  verticalSpace(30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.copyright,
+                        color: Colors.grey,
+                      ),
+                      horizontalSpace(5),
+                      Text(
+                        '2Way Deal Flutter Team',
+                        style: TextStyles.font13GreyBold,
+                      ),
+                    ],
+                  ),
+                  verticalSpace(30),
+                  AppTextButton(
+                    horizontalPadding: 10,
+                    verticalPadding: 10,
+                    buttonWidth: double.maxFinite,
+                    buttonText: 'About Us',
+                    textStyle: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                    onPressed: () {
+                      setState(() {
+                        aboutUs = !aboutUs;
+                      });
+                    },
+                  ),
+                  verticalSpace(20),
+                  aboutUs
+                      ? Column(
+                          children: [
+                            buildAnimatedTextItem(
+                              text: 'Two Way Deal Overview :-',
+                              textStyle: TextStyles.font17BlackBold,
+                            ),
+                            buildAnimatedTextItem(
+                              text:
+                                  'Our application aims to reduce food waste by selling it at lower price than its original cost or donating it. We strive to minimize food loss and promote affordability, benefiting both consumers and the environment.',
+                              textStyle: TextStyles.font15Black45Medium,
+                            ),
+                            verticalSpace(20),
+                            buildAnimatedTextItem(
+                              text: 'Our Goals :-',
+                              textStyle: TextStyles.font17BlackBold,
+                            ),
+                            buildAnimatedTextItem(
+                              text:
+                                  '1- Reduce food waste by 30% within the first year of implementation, as measured by the total weight of unsold food items.\n\n2- Increase the accessibility of affordable food options by offering discounted prices, leading to 20% increase in customer.\n\n3- participation within six months. Collaborate with local food banks and charities to donate surplus food, aiming to distribute a minimum of 500 meals per month to those in need.',
+                              textStyle: TextStyles.font15Black45Medium,
+                            ),
+                          ],
+                        )
+                      : Container(),
+                ],
+              ),
             ),
           ),
         ),
@@ -146,10 +136,10 @@ class _AboutAppScreenState extends State<AboutAppScreen> {
   }
 }
 
-Widget BuildAnimatedTextItem(
-    {required String text,
-    required TextStyle textStyle,
-    }) {
+Widget buildAnimatedTextItem({
+  required String text,
+  required TextStyle textStyle,
+}) {
   return SizedBox(
     width: double.infinity,
     child: DefaultTextStyle(
@@ -160,7 +150,7 @@ Widget BuildAnimatedTextItem(
       child: AnimatedTextKit(
         totalRepeatCount: 1,
         animatedTexts: [
-          TypewriterAnimatedText(text, textStyle: textStyle),
+          TypewriterAnimatedText(text, textStyle: textStyle, cursor: '...'),
         ],
         // onTap: () {
 
