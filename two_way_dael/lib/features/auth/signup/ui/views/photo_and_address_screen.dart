d897bbfd5f8d74661a70c7c39c510cf7bef06e9a
3laaOmran <115973437+3laaOmran@users.noreply.github.com>
2024-05-14 14:37:31 +0300

    change Address input

--- two_way_dael/lib/features/auth/signup/ui/views/photo_and_address_screen.dart
+++ two_way_dael/lib/features/auth/signup/ui/views/photo_and_address_screen.dart
@@ -1,15 +1,14 @@
+import 'package:drop_down_list/model/selected_list_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:two_way_dael/core/helpers/extensions.dart';
 import 'package:two_way_dael/core/helpers/spacing.dart';
 import 'package:two_way_dael/core/routing/routes.dart';
 import 'package:two_way_dael/core/theming/colors.dart';
 import 'package:two_way_dael/core/theming/styles.dart';
 import 'package:two_way_dael/core/widgets/custom_button.dart';
-import 'package:two_way_dael/core/widgets/resuable_text.dart';
+import 'package:two_way_dael/core/widgets/custom_drop_down_list.dart';
 import 'package:two_way_dael/features/auth/signup/logic/cubit/siginup_cubit.dart';
-import 'package:two_way_dael/features/auth/signup/ui/widgets/build_drop_down_list.dart';
 
 class PhotoAndAddressScreen extends StatelessWidget {
   const PhotoAndAddressScreen({super.key});
@@ -21,24 +20,25 @@ class PhotoAndAddressScreen extends StatelessWidget {
       child: BlocBuilder<SignupCubit, SignupStates>(
         builder: (context, state) {
           var cubit = SignupCubit.get(context);
-          List<String> governoratesList = [
-            'Cairo',
-            'Alexandria',
-            'ElBehira',
-            'Giza',
-            'Aswan',
-            'Banha',
+          TextEditingController governorateController = TextEditingController();
+          TextEditingController cityController = TextEditingController();
+          var formKey = GlobalKey<FormState>();
+          List<SelectedListItem> governoratesList = [
+            SelectedListItem(name: 'Cairo'),
+            SelectedListItem(name: 'Alexandria'),
+            SelectedListItem(name: 'ElBehira'),
+            SelectedListItem(name: 'Giza'),
+            SelectedListItem(name: 'Aswan'),
+            SelectedListItem(name: 'Banha'),
           ];
-          List<String> citiesList = [
-            'Abohomoss',
-            'El8Pzor',
-            'KafrEldawar',
-            'Balakter',
-            'Elgenawia',
-            'Dsoness',
+          List<SelectedListItem> citiesList = [
+            SelectedListItem(name: 'Abohomoss'),
+            SelectedListItem(name: 'El8Pzor'),
+            SelectedListItem(name: 'KafrEldawar'),
+            SelectedListItem(name: 'Balakter'),
+            SelectedListItem(name: 'Elgenawia'),
+            SelectedListItem(name: 'Dsoness'),
           ];
-          String selectedGovernorate = 'ElBehira';
-          String selectedCity = 'Balakter';
           return Container(
             decoration: const BoxDecoration(
               image: DecorationImage(
@@ -53,125 +53,151 @@ class PhotoAndAddressScreen extends StatelessWidget {
                 child: SizedBox(
                   width: double.infinity,
                   child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        verticalSpace(100),
-                        Text(
-                          'Last Step',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headlineMedium
-                              ?.copyWith(
-                                  fontWeight: FontWeight.w900, fontSize: 30.0),
-                        ),
-                        Text(
-                          'Add more details',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headlineSmall
-                              ?.copyWith(
-                                  fontWeight: FontWeight.bold, fontSize: 18.0),
-                        ),
-                        verticalSpace(40),
-                        Text(
-                          'Add Your Profile Photo',
-                          style: Theme.of(context)
-                              .textTheme
-                              .labelSmall
-                              ?.copyWith(
-                                  fontSize: 19.0, fontWeight: FontWeight.w100),
-                        ),
-                        verticalSpace(10),
-                        SizedBox(
-                          width: 250,
-                          height: 180,
-                          child: Stack(
-                            alignment: AlignmentDirectional.bottomEnd,
-                            children: [
-                              Positioned(
-                                top: 0,
-                                left: 0,
-                                child: Container(
-                                  width: 240.0,
-                                  height: 170.0,
+                    child: Form(
+                      key: formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          verticalSpace(100),
+                          Text(
+                            'Last Step',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineMedium
+                                ?.copyWith(
+                                    fontWeight: FontWeight.w900,
+                                    fontSize: 30.0),
+                          ),
+                          Text(
+                            'Add more details',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineSmall
+                                ?.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18.0),
+                          ),
+                          verticalSpace(40),
+                          Text(
+                            'Add Your Profile Photo',
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelSmall
+                                ?.copyWith(
+                                    fontSize: 19.0,
+                                    fontWeight: FontWeight.w100),
+                          ),
+                          verticalSpace(10),
+                          SizedBox(
+                            width: 250,
+                            height: 180,
+                            child: Stack(
+                              alignment: AlignmentDirectional.bottomEnd,
+                              children: [
+                                Positioned(
+                                  top: 0,
+                                  left: 0,
+                                  child: Container(
+                                    width: 240.0,
+                                    height: 170.0,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        image: cubit.imagePick == null
+                                            ? const DecorationImage(
+                                                image: AssetImage(
+                                                    'assets/images/image_picker_background.png'),
+                                                fit: BoxFit.none)
+                                            : DecorationImage(
+                                                image: FileImage(
+                                                    cubit.imagePick!)),
+                                        borderRadius: const BorderRadius.all(
+                                            Radius.circular(25.0)),
+                                        border: Border.all(
+                                            color: Colors.grey, width: 1.0)),
+                                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  ),
+                                ),
+                                Container(
                                   decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      image: cubit.imagePick == null
-                                          ? const DecorationImage(
-                                              image: AssetImage(
-                                                  'assets/images/image_picker_background.png'),
-                                              fit: BoxFit.none)
-                                          : DecorationImage(
-                                              image:
-                                                  FileImage(cubit.imagePick!)),
                                       borderRadius: const BorderRadius.all(
                                           Radius.circular(25.0)),
                                       border: Border.all(
-                                          color: Colors.grey, width: 1.0)),
-                                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                                ),
-                              ),
-                              Container(
-                                decoration: BoxDecoration(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(25.0)),
-                                    border: Border.all(
+                                          color: ColorManager.mainOrange,
+                                          width: 1.0)),
+                                  child: CircleAvatar(
+                                    radius: 17.0,
+                                    backgroundColor: Colors.white,
+                                    child: IconButton(
+                                      onPressed: () {
+                                        cubit.pickImage();
+                                      },
+                                      icon: const Icon(
+                                        Icons.add,
+                                        size: 20.0,
                                         color: ColorManager.mainOrange,
-                                        width: 1.0)),
-                                child: CircleAvatar(
-                                  radius: 17.0,
-                                  backgroundColor: Colors.white,
-                                  child: IconButton(
-                                    onPressed: () {
-                                      cubit.pickImage();
-                                    },
-                                    icon: const Icon(
-                                      Icons.add,
-                                      size: 20.0,
-                                      color: ColorManager.mainOrange,
+                                      ),
                                     ),
                                   ),
                                 ),
+                              ],
+                            ),
+                          ),
+                          verticalSpace(50),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CustomDropDownList(
+                                validation: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Address is required';
+                                  }
+                                  return null;
+                                },
+                                prefixIcon: const Icon(
+                                  Icons.location_city_outlined,
+                                  color: ColorManager.mainOrange,
+                                ),
+                                dropedList: governoratesList,
+                                textEditingController: governorateController,
+                                title: 'Governorate',
+                                hint: 'Governorate',
+                                isCitySelected: true,
+                              ),
+                              verticalSpace(20),
+                              CustomDropDownList(
+                                validation: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Address is required';
+                                  }
+                                  return null;
+                                },
+                                prefixIcon: const Icon(
+                                  Icons.location_on,
+                                  color: ColorManager.mainOrange,
+                                ),
+                                dropedList: citiesList,
+                                textEditingController: cityController,
+                                title: 'City',
+                                hint: 'City',
+                                isCitySelected: true,
                               ),
                             ],
                           ),
-                        ),
-                        verticalSpace(50),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            resuableText(
-                              text: "Governorate",
-                              fontsize: 17.sp,
-                            ),
-                            buildDropDownList(
-                              list: governoratesList,
-                              selectedItem:selectedGovernorate,
-                            ),
-                            verticalSpace(30),
-                            resuableText(
-                              text: "City",
-                              fontsize: 17.sp,
-                            ),
-                            buildDropDownList(
-                              list: citiesList,
-                              selectedItem:selectedCity,
-                            ),
-                          ],
-                        ),
-                        verticalSpace(50),
-                        AppTextButton(
-                          buttonText: 'Next',
-                          textStyle: TextStyles.font20Whitebold,
-                          onPressed: () {
-                            context.pushNamedAndRemoveUntil(
-                              Routes.homeScreen,
-                              predicate: (route) => false,
-                            );
-                          },
-                        ),
-                      ],
+                          verticalSpace(50),
+                          AppTextButton(
+                            buttonText: 'Next',
+                            textStyle: TextStyles.font20Whitebold,
+                            onPressed: () {
+                              if (formKey.currentState!.validate()) {
+                                context.pushNamedAndRemoveUntil(
+                                Routes.homeScreen,
+                                predicate: (route) => false,
+                              );
+                              }
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
--- two_way_dael/lib/features/home/ui/widgets/edit_info.dart
+++ two_way_dael/lib/features/home/ui/widgets/edit_info.dart
@@ -1,14 +1,16 @@
+import 'package:drop_down_list/model/selected_list_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:two_way_dael/core/helpers/spacing.dart';
 import 'package:two_way_dael/core/theming/colors.dart';
 import 'package:two_way_dael/core/theming/styles.dart';
+import 'package:two_way_dael/core/widgets/custom_drop_down_list.dart';
+import 'package:two_way_dael/core/widgets/resuable_text.dart';
 import 'package:two_way_dael/features/home/logic/cubit/customer_cubit.dart';
 import 'package:two_way_dael/features/home/logic/cubit/customer_states.dart';
 
 import '../../../../core/widgets/custom_button.dart';
 import '../../../../core/widgets/custom_text_form_field.dart';
-import '../../../auth/signup/ui/widgets/build_drop_down_list.dart';
 
 class EditInfo extends StatelessWidget {
   const EditInfo({super.key});
@@ -17,6 +19,8 @@ class EditInfo extends StatelessWidget {
     return BlocConsumer<CustomerCubit, CustomerStates>(
       listener: (context, state) {},
       builder: (context, state) {
+        TextEditingController governorateController = TextEditingController();
+        TextEditingController cityController = TextEditingController();
         var cubit = CustomerCubit.get(context);
         var model = cubit.userModel;
         cubit.nameController.text = model!.data!.name!;
@@ -33,10 +37,15 @@ class EditInfo extends StatelessWidget {
                         color: ColorManager.mainOrange,
                       )
                     : Text(
-                        'Edit Info',
+                        'Edit Profile',
                         style: TextStyles.font20blackbold,
                       ),
                 verticalSpace(30),
+                resuableText(
+                  text: 'User Name',
+                  fontsize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
                 CustomTextFormField(
                   prefixIcon: const Icon(Icons.person),
                   controller: cubit.nameController,
@@ -50,6 +59,11 @@ class EditInfo extends StatelessWidget {
                   },
                 ),
                 verticalSpace(20),
+                resuableText(
+                  text: 'Email Address',
+                  fontsize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
                 CustomTextFormField(
                   prefixIcon: const Icon(Icons.email),
                   keyboardType: TextInputType.emailAddress,
@@ -63,6 +77,11 @@ class EditInfo extends StatelessWidget {
                   },
                 ),
                 verticalSpace(20),
+                resuableText(
+                  text: 'Mobile Phone',
+                  fontsize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
                 CustomTextFormField(
                   prefixIcon: const Icon(Icons.phone),
                   keyboardType: TextInputType.phone,
@@ -78,18 +97,58 @@ class EditInfo extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    buildDropDownList(
-                      list: ['Cairo', 'Alex', 'Behira', 'Giza'],
-                      selectedItem: 'Cairo',
+                    CustomDropDownList(
+                      prefixIcon: const Icon(
+                        Icons.location_city_outlined,
+                        color: ColorManager.mainOrange,
+                      ),
+                      dropedList: [
+                        SelectedListItem(name: 'Cairo'),
+                        SelectedListItem(name: 'Behira'),
+                        SelectedListItem(name: 'Giza'),
+                        SelectedListItem(name: 'Alex'),
+                        SelectedListItem(name: 'Dakahlia'),
+                        SelectedListItem(name: 'Banhaa'),
+                        SelectedListItem(name: 'Cairo'),
+                        SelectedListItem(name: 'Behira'),
+                        SelectedListItem(name: 'Giza'),
+                        SelectedListItem(name: 'Alex'),
+                        SelectedListItem(name: 'Dakahlia'),
+                        SelectedListItem(name: 'Banhaa'),
+                      ],
+                      textEditingController: governorateController,
+                      title: 'Governorate',
+                      hint: 'Governorate',
+                      isCitySelected: true,
                     ),
-                    verticalSpace(15),
-                    buildDropDownList(
-                      list: ['Abohommos', 'Damanhoor', 'Nobaria'],
-                      selectedItem: 'Abohommos',
+                    verticalSpace(20),
+                    CustomDropDownList(
+                      prefixIcon: const Icon(
+                        Icons.location_on,
+                        color: ColorManager.mainOrange,
+                      ),
+                      dropedList: [
+                        SelectedListItem(name: 'Cairo'),
+                        SelectedListItem(name: 'Behira'),
+                        SelectedListItem(name: 'Giza'),
+                        SelectedListItem(name: 'Alex'),
+                        SelectedListItem(name: 'Dakahlia'),
+                        SelectedListItem(name: 'Banhaa'),
+                        SelectedListItem(name: 'Cairo'),
+                        SelectedListItem(name: 'Behira'),
+                        SelectedListItem(name: 'Giza'),
+                        SelectedListItem(name: 'Alex'),
+                        SelectedListItem(name: 'Dakahlia'),
+                        SelectedListItem(name: 'Banhaa'),
+                      ],
+                      textEditingController: cityController,
+                      title: 'City',
+                      hint: 'City',
+                      isCitySelected: true,
                     ),
                   ],
                 ),
-                verticalSpace(15),
+                verticalSpace(20),
                 AppTextButton(
                   buttonText: 'Change Password',
                   textStyle: const TextStyle(
